677c6080ab2de63f739946e5502cec58707fd0bb
vlads22 <vlad.curteanu@yahoo.com>
2024-07-29 22:38:15 +0200

    1

--- src/module4/Main.java
+++ src/module4/Main.java
@@ -2,7 +2,10 @@ package module4;
 
 public class Main {
     public static void main(String[] args) {
-        FirstCSVExample firstCSVExample = new FirstCSVExample();
-        firstCSVExample.readFood();
+//        FirstCSVExample firstCSVExample = new FirstCSVExample();
+//        firstCSVExample.readFood();
+
+        WhichCountriesExport whichCountriesExport = new WhichCountriesExport();
+        whichCountriesExport.tester();
     }
 }
--- src/module4/WhichCountriesExport.java
+++ src/module4/WhichCountriesExport.java
@@ -23,24 +23,73 @@ public class WhichCountriesExport {
     }
 
     public String countryInfo(CSVParser parser, String country) {
+
+        boolean countryFound = false;
+        String exports = "";
+        String value = "";
+
         for(CSVRecord record : parser) {
-            String countryName = record.get"
+            String countryName = record.get("Country");
+            if (countryName.contains(country)){
+                exports = record.get("Exports");
+                value = record.get("Value (dollars)");
+                countryFound = true;
+            }
+        }
+        if (countryFound = true) {
+            return country + ": " + exports + ": " + value;
+        }
+        else {
+            return "NOT FOUND";
         }
     }
 
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2) {
+        for(CSVRecord record : parser) {
+            String exports = record.get("Exports");
+            if (exports.contains(exportItem1) && exports.contains(exportItem2)) {
+                String country = record.get("Country");
+                System.out.println(country);
+            }
+        }
+    }
 
+    public int numberOfExporters(CSVParser parser, String exportItem) {
+        int numberOfExporters = 0;
+        for (CSVRecord record : parser) {
+            String exports = record.get("Exports");
+            if (exports.contains(exportItem)) {
+                numberOfExporters = numberOfExporters + 1;
+            }
+        }
+        return numberOfExporters;
+    }
+
+    public void bigExporters(CSVParser parser, String amount){
+        for (CSVRecord record : parser) {
+            String exportValue = record.get("Value (dollars)");
+            if (exportValue.length() > amount.length()) {
+                String countryName = record.get("Country");
+                System.out.println(countryName + " " + exportValue);
+            }
+        }
+    }
 
     public void tester() {
+        // three column headers labeled Country, Exports, and Value (dollars)
         FileResource fr = new FileResource();
-        CSVParser parser = fr.getCSVParser();
-        countryInfo(parser);
 
-        CSVParser parser = fr.getCSVParser();
+//        CSVParser parser = fr.getCSVParser();
+//        countryInfo(parser, "Nauru");
 
+//        CSVParser parser2 = fr.getCSVParser();
+//        listExportersTwoProducts(parser2, "gold", "diamonds");
 
-        CSVParser parser = fr.getCSVParser();
-
-
+        CSVParser parser3 = fr.getCSVParser();
+        numberOfExporters(parser3, "gold");
+//
+//        CSVParser parser4 = fr.getCSVParser();
+//        bigExporters(parser4, "$400,000,000");
 
     }
 }
